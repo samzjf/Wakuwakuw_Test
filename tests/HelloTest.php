7c79c00eacb0030edac3dbf405b9f6aed31fde3d
Samuel Deddy <samz_jf@yahoo.com>
2012-04-03 13:02:26 +0700

    Nonsense Tests

---
+++ tests/HelloTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class HelloTest extends PHPUnit_Framework_TestCase {
+
+    public function testHello()
+    {
+        $hello = 'hello';
+        $this->assertEquals('hello', $hello);
+    }
+}
